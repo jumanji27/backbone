1b9e454fcbd2702441b905321f77debdc1db0b86
Nikita Lebedev <nikita.lebedev@ostrovok.ru>
2014-05-27 19:10:03 +0400

    navigate qs

--- backbone.js
+++ backbone.js
@@ -1336,7 +1336,7 @@
 
     // Simple proxy to `Backbone.history` to save a fragment into the history.
     navigate: function(fragment, options) {
-      Backbone.history.navigate(fragment, options);
+      Backbone.history.navigate(fragment + location.search, options);
       return this;
     },
 
