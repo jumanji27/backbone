864230a28028d46e0579fd38ca249f4ee380f885
Nikita Lebedev <nikita.lebedev@ostrovok.ru>
2015-04-07 19:35:47 +0300

    qs fix

--- backbone.js
+++ backbone.js
@@ -1270,7 +1270,7 @@
 
     // Simple proxy to `Backbone.history` to save a fragment into the history.
     navigate: function(fragment, options) {
-      if (options && (options.qs || options.qs === '')) {
+      if (options && options.qs) {
         Backbone.history.navigate(fragment + '?' + options.qs, options);
       }
       else {
